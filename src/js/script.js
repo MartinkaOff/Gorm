4fb0c4183f01989fc6e26c21eaed891938ec9656
MartinkaOff <110162487+MartinkaOff@users.noreply.github.com>
2022-10-05 15:55:48 +0600

    Completed fourth screen and add Slider

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
     $('.creative__teams-view').on('click', function() {
         $('.our_work').fadeIn('slow')
+    });
+
+    $('.testimonials__wrapper').slick( {
+        dots: true,
+        prevArrow: '<a class="slick-prev"></a>',
+        nextArrow: '<a class="slick-next"></a>'
     })
 })
